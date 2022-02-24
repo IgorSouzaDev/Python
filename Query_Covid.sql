bf2fc9e2b8a6450aaa8a6051a2f4e24ddf6c053e
IgorSouzaDev <igorpaulino0945@gmail.com>
2022-02-24 19:39:09 -0300

    Alteração das Querys

--- Query_Covid.sql
+++ Query_Covid.sql
@@ -1,10 +1,16 @@
--- Selecionar o Estado e a cidade com mais casos confirmados
-SELECT  state,city,max(confirmed) 
+-- Selecionar oS 10 Estado e a cidade com mais casos confirmados
+SELECT  state,city,max(last_available_confirmed) Confirmados
 FROM covid.stg_covid
-GROUP BY state,city 
-order by max desc limit 1
+GROUP BY state,city
+ORDER BY Confirmados DESC LIMIT 10
 
---Calcular o total de casos confirmados por estado
-SELEC state,confirmed 
+
+--Calcular os casos confirmados  por estado
+SELECT state,SUM(last_available_confirmed) Confirmados
 FROM covid.stg_covid
+GROUP BY state
+ORDER BY Confirmados DESC
+
+--Messes que tiveram mais casos confirmados
+
 
